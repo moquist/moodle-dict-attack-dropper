e21f5bfe7efdab72ce5cae5042df7a0d9ed7d3d4
Matt Oquist <moquist@majen.net>
2013-05-04 21:38:26 -0400

    first production version

---
+++ config-dist.py
@@ -0,0 +1,5 @@
+db_host = 'somehost'
+db_name = 'somedb'
+db_username = 'someuser'
+db_pw = 'secret'
+selectors = {'timewindow':60, 'failurelimit':20}
---
+++ moodle-dict-attack-dropper.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# Detect dictionary attacks against Moodle and drop packets from the perpetrators' IP addresses.
+
+from netfilter.rule import Rule
+from netfilter.table import Table
+import psycopg2
+import re
+import config
+
+def db_execute(conn, query, args=None):
+    cur = conn.cursor()
+    cur.execute(query, args)
+    return cur.fetchall()
+
+def get_offending_ips(conn, selectors):
+    """Get a list of IPs from the mdl_log table that have more than
+       failurelimit authentication failures within the past timewindow seconds"""
+    query = """
+        SELECT ip
+        FROM mdl_log
+        WHERE module = 'login'
+        AND action = 'error'
+        AND time > (EXTRACT (EPOCH FROM NOW()) - %(timewindow)s)
+        GROUP BY ip
+        HAVING COUNT(*) > %(failurelimit)s
+    """
+    return db_execute(conn, query, selectors)
+
+def block_ips(ips):
+    """For each of the listed source IP addresses (excluding private networks),
+       add a netfilter rule to DROP all its packets."""
+    # http://stackoverflow.com/questions/2814002/private-ip-address-identifier-in-regular-expression
+    private_ips = "^(127|10|172\.1[6-9]|172\.2[0-9]|172\.3[0-1]|192\.168)\."
+    for ip, in ips:
+        if not re.match(private_ips, ip):
+            print "Dropping all packets from %s" % ip
+            rule = Rule(source=ip, jump='DROP')
+            table = Table('filter')
+            table.append_rule('INPUT', rule)
+
+if __name__=="__main__":
+    conn = psycopg2.connect(host = config.db_host, database = config.db_name, user = config.db_username, password = config.db_pw)
+    block_ips(get_offending_ips(conn, config.selectors))
